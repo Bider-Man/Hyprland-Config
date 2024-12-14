f8e27daa8045b2f263f6ed925ae59adfaca72825
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-14 10:56:05 +0000

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -104,7 +104,6 @@ sudo pacman -Syu --noconfirm \
 
 yay -Syu --noconfirm \
     hyprsunset \
-    uwsm 
 
 # Create Rofi configuration
 echo "Creating Rofi configuration directory and dumping default config..."
