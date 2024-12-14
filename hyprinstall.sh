cbbf006efafb5948f09b47faa467ed018995d1ea
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-14 13:18:23 +0000

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -3,12 +3,11 @@
 # Define target directories
 CONFIG_DIR="$HOME/.config/hypr"
 HYPRLAND_DIR="$CONFIG_DIR/hyprland"
+HYPRLOCK_DIR="$CONFIG_DIR/hyprlock"
 SCRIPTS_DIR="$CONFIG_DIR/scripts"
 WAYBAR_DIR="$HOME/.config/waybar"
 FOOT_DIR="$HOME/.config/foot"
-HYPRLOCK_DIR="$CONFIG_DIR/hyprlock"
 WAYBAR_SRC_DIR="./hypr/waybar"
-HYPRLOCK_SRC_DIR="./hypr/hyprlock"
 
 # Clean up old configurations
 echo "Cleaning up old configurations in $CONFIG_DIR..."
@@ -25,21 +24,22 @@ else
   exit 1
 fi
 
-# Copy hyprlock folder
-if [ -d "$HYPRLOCK_SRC_DIR" ]; then
-  echo "Copying hyprlock folder to $HYPRLOCK_DIR..."
-  cp -r "$HYPRLOCK_SRC_DIR" "$HYPRLOCK_DIR/"
+# Copy scripts folder
+if [ -d "./hypr/scripts" ]; then
+  echo "Copying scripts to $SCRIPTS_DIR..."
+  cp -r ./hypr/scripts/* "$SCRIPTS_DIR/"
 else
-  echo "Error: ./hypr/hyprlock directory not found!"
+  echo "Error: ./hypr/scripts directory not found!"
   exit 1
 fi
 
-# Copy scripts folder
-if [ -d "./hypr/scripts" ]; then
-  echo "Copying additional scripts to $SCRIPTS_DIR..."
-  cp -r ./hypr/scripts/* "$SCRIPTS_DIR/"
+# Copy hyprlock folder
+if [ -d "./hypr/hyprlock" ]; then
+  echo "Copying hyprlock folder to $HYPRLOCK_DIR..."
+  cp -r ./hypr/hyprlock/* "$HYPRLOCK_DIR/"
 else
-  echo "Warning: ./hypr/scripts directory not found! Skipping additional scripts."
+  echo "Error: ./hypr/hyprlock directory not found!"
+  exit 1
 fi
 
 # Overwrite the auto-generated hyprland.conf
@@ -51,7 +51,15 @@ else
   exit 1
 fi
 
-# Copy hypridle.conf
+# Copy hyprlock.conf and hypridle.conf
+if [ -f "./hypr/hyprlock.conf" ]; then
+  echo "Copying hyprlock.conf to $CONFIG_DIR..."
+  cp ./hypr/hyprlock.conf "$CONFIG_DIR/hyprlock.conf"
+else
+  echo "Error: Provided hyprlock.conf file not found in ./hypr!"
+  exit 1
+fi
+
 if [ -f "./hypr/hypridle.conf" ]; then
   echo "Copying hypridle.conf to $CONFIG_DIR..."
   cp ./hypr/hypridle.conf "$CONFIG_DIR/hypridle.conf"
@@ -87,13 +95,8 @@ sudo pacman -Syu --noconfirm \
     ttf-arimo-nerd \
     noto-fonts-emoji \
     noto-fonts \
-    foot \
-    waybar \
-    dunst \
-    xdg-desktop-portal-hyprland \
-    swww \
-    pasystray \
-    fcitx5 \
+    noto-fonts-cjk \
+    otf-ipafont \
     swaylock \
     swayidle \
     wl-clipboard \
@@ -102,10 +105,11 @@ sudo pacman -Syu --noconfirm \
     procps-ng \
     hypridle \
     hyprpaper \
-    hyprlock
+    hyprlock \
+    fcitx5
 
 yay -Syu --noconfirm \
-    hyprsunset \
+    hyprsunset
 
 # Create Rofi configuration
 echo "Creating Rofi configuration directory and dumping default config..."
@@ -132,7 +136,6 @@ fi
 echo "Setting permissions for configuration files..."
 chmod -R 755 "$CONFIG_DIR"
 chmod +x "$HYPRLOCK_DIR/"*.sh
-chmod +x "$SCRIPTS_DIR/"*
 
 # Restart Waybar and Hyprland to apply the new configurations
 echo "Restarting Waybar and Hyprland..."
