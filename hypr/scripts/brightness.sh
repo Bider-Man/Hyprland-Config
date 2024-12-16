3daa4b3d57ca15795585bef0b3cadc6216dd1f73
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-16 21:02:31 +0530

    Update brightness.sh

--- hypr/scripts/brightness.sh
+++ hypr/scripts/brightness.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-brightness=$(brightnessctl get)
-max_brightness=$(brightnessctl max)
-percentage=$((100 * brightness / max_brightness))
-dunstify -u low -r 9998 -h int:value:$percentage "Brightness" "$percentage%"
+
+# Notify when brightness is increased
+brightness_up() {
+    current_brightness=$(brightnessctl g | awk '{print int($1/100*100)}')
+    notify-send "Brightness Up" "Current brightness: $current_brightness%" -i display-brightness-high
+}
+
+# Notify when brightness is decreased
+brightness_down() {
+    current_brightness=$(brightnessctl g | awk '{print int($1/100*100)}')
+    notify-send "Brightness Down" "Current brightness: $current_brightness%" -i display-brightness-low
+}
+
+# Detect the type of brightness action
+case "$1" in
+    "brightness-up")
+        brightness_up
+        ;;
+    "brightness-down")
+        brightness_down
+        ;;
+    *)
+        echo "Invalid argument. Use 'brightness-up' or 'brightness-down'."
+        ;;
+esac
