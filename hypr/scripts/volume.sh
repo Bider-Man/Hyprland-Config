e7d447aca1badf60ff6cbab324b349befcaa2815
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-21 16:49:59 +0000

    Update volume.sh

--- hypr/scripts/volume.sh
+++ hypr/scripts/volume.sh
@@ -1,39 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Notify when the volume is increased
-volume_up() {
-    current_volume=$(amixer get Master | grep -oP '\d+%' | head -n 1)
-    notify-send "Volume Up" "Current volume: $current_volume" -i audio-volume-high
+ScrDir=`dirname $(realpath $0)`
+source $ScrDir/globalcontrol.sh
+
+
+# define functions
+
+function print_error
+{
+cat << "EOF"
+    ./volumecontrol.sh -[device] <action>
+    ...valid device are...
+        i -- [i]nput decive
+        o -- [o]utput device
+    ...valid actions are...
+        i -- <i>ncrease volume [+5]
+        d -- <d>ecrease volume [-5]
+        m -- <m>ute [x]
+EOF
 }
 
-# Notify when the volume is decreased
-volume_down() {
-    current_volume=$(amixer get Master | grep -oP '\d+%' | head -n 1)
-    notify-send "Volume Down" "Current volume: $current_volume" -i audio-volume-low
+function notify_vol
+{
+    vol=`pamixer $srce --get-volume | cat`
+    angle="$(( (($vol+2)/5) * 5 ))"
+    ico="${icodir}/vol-${angle}.svg"
+    bar=$(seq -s "." $(($vol / 15)) | sed 's/[0-9]//g')
+    dunstify "t2" -a "$vol$bar" "$nsink" -i $ico -r 91190 -t 800
 }
 
-# Notify when the volume is muted
-volume_mute() {
-    current_mute_status=$(amixer get Master | grep -oP '\[.*\]' | head -n 1)
-    if [[ $current_mute_status == "[off]" ]]; then
-        notify-send "Volume Muted" -i audio-volume-muted
+function notify_mute
+{
+    mute=`pamixer $srce --get-mute | cat`
+    if [ "$mute" == "true" ] ; then
+        dunstify "t2" -a "muted" "$nsink" -i ${icodir}/muted-${dvce}.svg -r 91190 -t 800
     else
-        notify-send "Volume Unmuted" -i audio-volume-high
+        dunstify "t2" -a "unmuted" "$nsink" -i ${icodir}/unmuted-${dvce}.svg -r 91190 -t 800
     fi
 }
 
-# Detect the type of volume action
-case "$1" in
-    "volume-up")
-        volume_up
-        ;;
-    "volume-down")
-        volume_down
-        ;;
-    "volume-mute")
-        volume_mute
-        ;;
-    *)
-        echo "Invalid argument. Use 'volume-up', 'volume-down', or 'volume-mute'."
-        ;;
+
+# set device source
+
+while getopts io SetSrc
+do
+    case $SetSrc in
+    i) nsink=$(pamixer --list-sources | grep "_input." | head -1 | awk -F '" "' '{print $NF}' | sed 's/"//')
+        srce="--default-source"
+        dvce="mic" ;;
+    o) nsink=$(pamixer --get-default-sink | grep "_output." | awk -F '" "' '{print $NF}' | sed 's/"//')
+        srce=""
+        dvce="speaker" ;;
+    esac
+done
+
+if [ $OPTIND -eq 1 ] ; then
+    print_error
+fi
+
+
+# set device action
+
+shift $((OPTIND -1))
+step="${2:-5}"
+icodir="~/.config/dunst/icons/vol"
+
+case $1 in
+    i) pamixer $srce -i ${step}
+        notify_vol ;;
+    d) pamixer $srce -d ${step}
+        notify_vol ;;
+    m) pamixer $srce -t
+        notify_mute ;;
+    *) print_error ;;
 esac
