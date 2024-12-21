840a0e7b68c2729702b7b8c9dde066c842df75b5
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-21 16:58:41 +0000

    Update volume.sh

--- hypr/scripts/volume.sh
+++ hypr/scripts/volume.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env sh
 
-ScrDir=`dirname $(realpath $0)`
-source $ScrDir/globalcontrol.sh
-
-
-# define functions
-
-function print_error
-{
+# Define functions
+function print_error {
 cat << "EOF"
     ./volume.sh -[device] <action>
     ...valid device are...
-        i -- [i]nput decive
+        i -- [i]nput device
         o -- [o]utput device
     ...valid actions are...
         i -- <i>ncrease volume [+5]
@@ -20,37 +14,37 @@ cat << "EOF"
 EOF
 }
 
-function notify_vol
-{
-    vol=`pamixer $srce --get-volume | cat`
-    angle="$(( (($vol+2)/5) * 5 ))"
-    ico="${icodir}/vol-${angle}.svg"
-    bar=$(seq -s "." $(($vol / 15)) | sed 's/[0-9]//g')
-    dunstify "t2" -a "$vol$bar" "$nsink" -i $ico -r 91190 -t 800
+function notify_vol {
+    # Get the current volume
+    vol=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
+    # Calculate the volume level for the progress bar
+    bar=$(seq -s "." $(($vol / 5)) | sed 's/[0-9]//g')
+    # Use swaync to show the volume notification with a progress bar
+    swaync -m "Volume: $vol%" -p "[$bar]" -t 1
 }
 
-function notify_mute
-{
-    mute=`pamixer $srce --get-mute | cat`
-    if [ "$mute" == "true" ] ; then
-        dunstify "t2" -a "muted" "$nsink" -i ${icodir}/muted-${dvce}.svg -r 91190 -t 800
+function notify_mute {
+    # Get the mute status
+    mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+    if [ "$mute" == "yes" ]; then
+        # Send a mute notification with swaync
+        swaync -m "Muted" -i "/path/to/muted-icon.png" -t 1
     else
-        dunstify "t2" -a "unmuted" "$nsink" -i ${icodir}/unmuted-${dvce}.svg -r 91190 -t 800
+        # Send an unmute notification with swaync
+        swaync -m "Unmuted" -i "/path/to/unmuted-icon.png" -t 1
     fi
 }
 
-
-# set device source
-
+# Set device source
 while getopts io SetSrc
 do
     case $SetSrc in
-    i) nsink=$(pamixer --list-sources | grep "_input." | head -1 | awk -F '" "' '{print $NF}' | sed 's/"//')
-        srce="--default-source"
-        dvce="mic" ;;
-    o) nsink=$(pamixer --get-default-sink | grep "_output." | awk -F '" "' '{print $NF}' | sed 's/"//')
-        srce=""
-        dvce="speaker" ;;
+    i) nsink=$(pactl list short sources | grep "input" | head -n 1 | awk '{print $2}')
+       srce="--default-source"
+       dvce="mic" ;;
+    o) nsink=$(pactl list short sinks | grep "output" | head -n 1 | awk '{print $2}')
+       srce=""
+       dvce="speaker" ;;
     esac
 done
 
@@ -58,19 +52,17 @@ if [ $OPTIND -eq 1 ] ; then
     print_error
 fi
 
-
-# set device action
-
+# Set device action
 shift $((OPTIND -1))
 step="${2:-5}"
 icodir="~/.config/dunst/icons/vol"
 
 case $1 in
-    i) pamixer $srce -i ${step}
+    i) pactl set-sink-volume $nsink +${step}%  # Increase volume
         notify_vol ;;
-    d) pamixer $srce -d ${step}
+    d) pactl set-sink-volume $nsink -${step}%  # Decrease volume
         notify_vol ;;
-    m) pamixer $srce -t
+    m) pactl set-sink-mute $nsink toggle  # Toggle mute
         notify_mute ;;
     *) print_error ;;
 esac
