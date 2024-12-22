65349673139107bb03fb2c5f60401f27de9a7e19
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-22 23:59:42 +0530

    Update wallpaper_chooser.sh

--- hypr/scripts/wallpaper_chooser.sh
+++ hypr/scripts/wallpaper_chooser.sh
@@ -3,14 +3,14 @@
 # Specify the default folder (adjust as needed)
 default_folder="$HOME/Pictures"
 
-# Open Zenity file picker to select an image file
+# Use zenity to select a wallpaper file
 wallpaper=$(zenity --file-selection --title="Select a Wallpaper" --file-filter="Image files|*.jpg;*.jpeg;*.png;*.bmp;*.gif" --directory="$default_folder")
 
-# Check if the user selected a file
+# Check if a file was selected
 if [ -n "$wallpaper" ]; then
-  # Set the selected wallpaper using swww
+  # Set the wallpaper using swww
   swww img "$wallpaper"
   echo "Wallpaper changed to: $wallpaper"
 else
-  echo "No wallpaper selected."
+  echo "No wallpaper selected or dialog was canceled."
 fi
