084588e18ef25917fbdf1b64b18fe6aebdfe499b
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-22 19:21:41 +0530

    Update volume.sh

--- hypr/scripts/volume.sh
+++ hypr/scripts/volume.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# Get volume percentage
+# Get the current volume level
 volume=$(pactl list sinks | grep -E '^\s*Volume' | awk '{print $5}' | sed 's/%//')
 
-# Determine action (increase, decrease, mute)
+# Define volume control actions
 case $1 in
-    up) pactl set-sink-volume @DEFAULT_SINK@ +5% ;;
-    down) pactl set-sink-volume @DEFAULT_SINK@ -5% ;;
-    mute) pactl set-sink-mute @DEFAULT_SINK@ toggle ;;
+    up)
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
+        volume=$((volume + 5))
+        if [ "$volume" -gt 100 ]; then volume=100; fi
+        ;;
+    down)
+        pactl set-sink-volume @DEFAULT_SINK@ -5%
+        volume=$((volume - 5))
+        if [ "$volume" -lt 0 ]; then volume=0; fi
+        ;;
+    mute)
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
+        if [ $(pactl list sinks | grep -E '^\s*Mute' | awk '{print $2}') == "yes" ]; then
+            volnoti-show -m
+            exit 0
+        fi
+        ;;
+    *)
+        echo "Invalid option"
+        exit 1
+        ;;
 esac
 
-# Show notification with volnoti
+# Show volume notification using volnoti
 volnoti-show $volume
