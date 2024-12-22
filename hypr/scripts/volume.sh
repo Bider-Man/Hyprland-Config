72c155518059f69da347b7894f37e2f6b75c8eb2
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-22 19:18:49 +0530

    Update volume.sh

--- hypr/scripts/volume.sh
+++ hypr/scripts/volume.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Get the current volume percentage
+# Get volume percentage
 volume=$(pactl list sinks | grep -E '^\s*Volume' | awk '{print $5}' | sed 's/%//')
 
-# Check if muted
-muted=$(pactl list sinks | grep -E '^\s*Mute' | awk '{print $2}')
+# Determine action (increase, decrease, mute)
+case $1 in
+    up) pactl set-sink-volume @DEFAULT_SINK@ +5% ;;
+    down) pactl set-sink-volume @DEFAULT_SINK@ -5% ;;
+    mute) pactl set-sink-mute @DEFAULT_SINK@ toggle ;;
+esac
 
 # Show notification with volnoti
-if [ "$muted" == "yes" ]; then
-    volnoti-show -m
-else
-    volnoti-show $volume
-fi
+volnoti-show $volume
