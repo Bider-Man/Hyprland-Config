926d448f317ebe0c33302234c3836fd34722e4f6
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-22 18:31:45 +0530

    Update brightness.sh

--- hypr/scripts/brightness.sh
+++ hypr/scripts/brightness.sh
@@ -1,61 +1,76 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Define functions
-function print_error {
-cat << "EOF"
-    ./brightness.sh -[device] <action>
-    ...valid device are...
-        b -- [b]rightness device
-    ...valid actions are...
-        i -- <i>ncrease brightness [+10]
-        d -- <d>ecrease brightness [-10]
-EOF
-}
+# Set icon directory
+iDIR="$HOME/.config/hypr/mako/icons"
 
 # Initialize notification ID
 NOTIFY_ID="brightness_notification"
 
-function notify_brightness {
-    brightness=$(brightnessctl g | awk '{print int($1/100*100)}')  # Get current brightness in percentage
-    max_brightness=$(brightnessctl m)  # Get the maximum brightness
-    bar=$(seq -s "." $(($brightness / 10)) | sed 's/[0-9]//g')  # Build the progress bar with dots
-    
-    # Use font-based symbols for brightness
-    if [ "$brightness" -ge 66 ]; then
-        icon="☀️"  # Full brightness
-    elif [ "$brightness" -ge 33 ]; then
-        icon="🌞"  # Medium brightness
-    else
-        icon="🌑"  # Low brightness
-    fi
-
-    # Send the notification using notify-send
-    notify-send -h int:transient:1 -h string:x-canonical-private-synchronous:"$NOTIFY_ID" \
-                "$icon Brightness: $brightness%" "$bar"
+# Get current brightness
+get_backlight() {
+    echo $(brightnessctl -m | cut -d, -f4)
 }
 
-# Set device source
-while getopts b SetSrc
+# Get the corresponding icon based on brightness
+get_icon() {
+    current=$(get_backlight | sed 's/%//')
+    if [ "$current" -le "20" ]; then
+        icon="$iDIR/brightness-20.png"
+    elif [ "$current" -le "40" ]; then
+        icon="$iDIR/brightness-40.png"
+    elif [ "$current" -le "60" ]; then
+        icon="$iDIR/brightness-60.png"
+    elif [ "$current" -le "80" ]; then
+        icon="$iDIR/brightness-80.png"
+    else
+        icon="$iDIR/brightness-100.png"
+    fi
+}
+
+# Send the notification
+notify_user() {
+    current=$(get_backlight)
+    notify-send -h int:transient:1 -h string:x-canonical-private-synchronous:"$NOTIFY_ID" \
+                -u low -i "$icon" "Brightness : $current%"
+}
+
+# Change brightness
+change_backlight() {
+    brightnessctl set "$1" && get_icon && notify_user
+}
+
+# Function to print error if no valid action is provided
+print_error() {
+    cat << "EOF"
+    ./brightness.sh -[device] <action>
+    ...valid actions are...
+        i -- <i>ncrease brightness [+10]
+        d -- <d>ecrease brightness [-10]
+        g -- <g>et current brightness
+EOF
+}
+
+# Set device action
+while getopts "b" SetSrc
 do
     case $SetSrc in
-    b) nsink="brightnessctl"  # Use brightnessctl for brightness control
-       srce=""
-       dvce="brightness" ;;
+    b) nsink="brightnessctl" ;;
     esac
 done
 
+# If no option provided, print error
 if [ $OPTIND -eq 1 ] ; then
     print_error
+    exit 1
 fi
 
-# Set device action
+# Set action for brightness adjustment
 shift $((OPTIND -1))
 step="${2:-10}"
 
 case $1 in
-    i) brightnessctl s +${step}%  
-        notify_brightness ;;
-    d) brightnessctl s -${step}%  
-        notify_brightness ;;
+    i) change_backlight "+${step}%" ;;
+    d) change_backlight "${step}%-" ;;
+    g) get_backlight ;;
     *) print_error ;;
 esac
