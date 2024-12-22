ba073d008c28d316522ebe7d54aa5f2ff1d37fa4
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-23 00:14:45 +0530

    Update wallpaper_chooser.sh

--- hypr/scripts/wallpaper_chooser.sh
+++ hypr/scripts/wallpaper_chooser.sh
@@ -4,13 +4,13 @@
 default_folder="$HOME/Pictures"
 
 # Open ranger with foot terminal and allow the user to select a file
-wallpaper=$(foot -e ranger --choosefile="$default_folder")
+wallpaper=$(foot -e ranger --choosefile="$default_folder" | tail -n 1)
 
 # Check if a file was selected
-if [ -n "$wallpaper" ]; then
+if [ -f "$wallpaper" ]; then
   # Set the wallpaper using swww
   swww img "$wallpaper"
   echo "Wallpaper changed to: $wallpaper"
 else
-  echo "No wallpaper selected or dialog was canceled."
+  echo "No valid wallpaper selected or dialog was canceled."
 fi
