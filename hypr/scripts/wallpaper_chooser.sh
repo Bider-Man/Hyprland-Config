2f9c48efaba76487dca96e503b707c758f2b21bb
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-22 23:17:26 +0530

    Update wallpaper_chooser.sh

--- hypr/scripts/wallpaper_chooser.sh
+++ hypr/scripts/wallpaper_chooser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Open the file manager to choose the wallpaper
-chosen_wallpaper=$(zenity --file-selection --title="Select Wallpaper" --file-filter="Images|*.jpg;*.png;*.jpeg;*.bmp;*.gif")
+# Open Thunar to choose the wallpaper
+chosen_wallpaper=$(thunar --no-desktop --action="open" | zenity --file-selection --title="Select Wallpaper" --file-filter="Images|*.jpg;*.png;*.jpeg;*.bmp;*.gif")
 
 # Check if a file was chosen
 if [ -n "$chosen_wallpaper" ]; then
