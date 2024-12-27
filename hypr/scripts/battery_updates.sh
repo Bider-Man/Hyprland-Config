f6c7789be4a9c23fbd40a1cd0a055a1c8e540f15
Bider-Man <karthikpolpakkara@gmail.com>
2024-12-27 13:54:06 +0530

    Update battery_updates.sh

--- hypr/scripts/battery_updates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the battery capacity
-capacity=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i percentage | awk '{print $2}' | sed 's/%//')
-
-# Output the capacity in a format usable by Waybar
-echo "{ \"capacity\": \"$capacity\" }"
