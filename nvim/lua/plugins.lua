e02b548df4fb3bf38d9fc99d232ded694e8cfe60
Bider-Man <karthikpolpakkara@gmail.com>
2025-05-24 03:15:12 +0200

    Nvim Tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -95,6 +95,40 @@ plugins = {
 	    	vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
 	    	vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
 		end
+	},
+
+	---- FILE EXPLORER ----
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {
+				view = {
+					width = 35,
+				},
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
+					local function opts(desc)
+						return { desc = "File Tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+					end
+
+					vim.keymap.set("n", "<leader>f", api.tree.toggle, opts("Toggle"))
+					vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+					vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+					vim.keymap.set("n", "a", api.fs.create, opts("Create File/Directory"))
+					vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+					vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+					vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+					vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+					vim.keymap.set("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+					vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+				end,
+			}
+		end
 	}
 }
 
