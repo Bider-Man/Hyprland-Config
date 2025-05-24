f860f82b050a7a9171def48b14f4154e75cc3e34
Bider-Man <karthikpolpakkara@gmail.com>
2025-05-24 03:36:53 +0200

    NVIM Note taking

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,3 +8,7 @@ require("plugins")
 --Loading Color Scheme (Tokyo Night)
 require("colorscheme")
 
+--Loading Telekasten
+require("telekasten").setup({
+	home = vim.fn.expand("~/SRH"),
+})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -129,6 +129,46 @@ plugins = {
 				end,
 			}
 		end
+	},
+
+	---- MASON ----
+	{
+		"williamboman/mason.nvim",
+		opts = { ensure_installed = { "tree-sitter-cli" } },
+	},
+
+	---- NOTETAKING ----
+	{
+		"jbyuki/nabla.nvim",
+		dependencies = {
+			"nvim-neo-tree/neo-tree.nvim",
+			"williamboman/mason.nvim",
+		},
+		lazy = true,
+
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "latex" },
+				auto_install = true,
+				sync_install = false,
+			})
+		end,
+
+		keys = function()
+			return {
+				{
+					"<leader>p",
+					':lua require("nabla").popup()<cr>',
+					desc = "NablaPopUp",
+				},
+			}
+		end,
+	},
+
+	---- TELEKASTEN ----
+	{
+		"renerocksai/telekasten.nvim",
+		dependencies = {"nvim-telescope/telescope.nvim"}
 	}
 }
 
